5a939a110403fc5cc4562034defc0f78fa1528e8
alice-thaitea <athai026@ucr.edu>
2021-05-04 11:40:47 -0700

    Wrote and test, filter Age 1 and 2

--- select_and_test.hpp
+++ select_and_test.hpp
@@ -35,4 +35,18 @@ TEST(SelectAnd, Last_Cornell_Dang) {
     EXPECT_EQ(out.str(), "");
 }
 
+TEST(SelectAnd, Age_1_2) {
+    Spreadsheet test_sheet;
+    test_sheet.set_column_names({"First", "Last", "Age", "Major"});
+    test_sheet.add_row({"Angel", "Adams", "19", "English"});
+    test_sheet.add_row({"Charles", "Cornell", "21", "Music"});
+    test_sheet.add_row({"Lauren", "Dang", "20", "Film"});
+    test_sheet.add_row({"Amy", "Adams", "18", "English"});
+ 
+    std::ostringstream out;
+    test_sheet.set_selection(new Select_And(new Select_Contains(&test_sheet, "Age", "1"), new Select_Contains(&test_sheet, "Age", "2")));
+    test_sheet.print_selection(out);
+    EXPECT_EQ(out.str(), "Charles Cornell 21 Music \n");
+}
+
 #endif //__SELECT_AND_TEST_HPP__
