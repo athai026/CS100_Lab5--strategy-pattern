a440f97ecbdeb42a3a4fefa9d30a3b898b8813f8
AEssaian740 <aessaian101@gmail.com>
2021-05-03 23:16:27 -0700

    Added select_not

---
+++ selectNot.hpp
@@ -0,0 +1,24 @@
+#define __SELECT_NOT_HPP__
+
+#include "select.hpp"
+#include <cstring>
+
+class Select_Not
+{
+protected:
+Select* select;
+public:
+     Select_Not(Select* _select)
+     {
+          delete select;
+
+          select = _select;
+     }
+
+     virtual bool select() const
+     {
+          return !select.select();
+     }
+};
+
+#endif //__SELECT_NOT_HPP__
