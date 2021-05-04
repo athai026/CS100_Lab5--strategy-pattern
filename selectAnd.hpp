e2b5866cfd9d20effaa7ec138b941e1178ac053c
AEssaian740 <aessaian101@gmail.com>
2021-05-03 22:36:27 -0700

    Created Select_And

---
+++ selectAnd.hpp
@@ -0,0 +1,28 @@
+#define __SELECT_AND_HPP__
+#include <select.hpp>
+#include <cstring>
+
+class Select_And
+{
+protected:
+Select* select1;
+Select* select2;
+public:
+    Select_And(Select* _select1, Select* _select2)
+    {
+         delete  select1;
+         delete  select2;
+   
+         select1 = _select1;
+         select2 = _select2;
+    }
+
+    virtual bool select() const
+    {
+         if(select1.select() == select2.select()) {
+            return select1.select();
+        } else {return false;}
+    }
+};
+
+#endif //__SELECT_AND_HPP__
