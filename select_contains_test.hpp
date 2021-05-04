2338e7b82452a97c8f277f555e4dfa17c5236498
alice-thaitea <athai026@ucr.edu>
2021-05-04 01:56:31 -0700

    Wrote contains test, filter Last o

--- select_contains_test.hpp
+++ select_contains_test.hpp
@@ -49,5 +49,19 @@ TEST(SelectContains, First_allnames) {
     EXPECT_EQ(out.str(), "Angel Adams 19 English \nCharles Cornell 21 Music \nLauren Dang 20 Film \nAmy Adams 19 English \n");
 }
 
+TEST(SelectContains, Last_o) {
+    Spreadsheet test_sheet;
+    test_sheet.set_column_names({"First", "Last", "Age", "Major"});
+    test_sheet.add_row({"Angel", "Adams", "19", "English"});
+    test_sheet.add_row({"Charles", "Cornell", "21", "Music"});
+    test_sheet.add_row({"Lauren", "Dang", "20", "Film"});
+    test_sheet.add_row({"Amy", "Adams", "19", "English"});
+ 
+    std::ostringstream out;
+    test_sheet.set_selection(new Select_Contains(&test_sheet, "Last", "o"));
+    test_sheet.print_selection(out);
+    EXPECT_EQ(out.str(), "Charles Cornell 21 Music \n");
+}
+
 
 #endif //__SELECT_CONTAINS_TEST_HPP__
