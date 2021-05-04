95ad85ac5635e63a9dcbe065eb8c306ff1ec1b8a
alice-thaitea <athai026@ucr.edu>
2021-05-04 01:57:27 -0700

    Wrote contains test, filter Age 19

--- select_contains_test.hpp
+++ select_contains_test.hpp
@@ -63,5 +63,19 @@ TEST(SelectContains, Last_o) {
     EXPECT_EQ(out.str(), "Charles Cornell 21 Music \n");
 }
 
+TEST(SelectContains, Age_19) {
+    Spreadsheet test_sheet;
+    test_sheet.set_column_names({"First", "Last", "Age", "Major"});
+    test_sheet.add_row({"Angel", "Adams", "19", "English"});
+    test_sheet.add_row({"Charles", "Cornell", "21", "Music"});
+    test_sheet.add_row({"Lauren", "Dang", "20", "Film"});
+    test_sheet.add_row({"Amy", "Adams", "19", "English"});
+ 
+    std::ostringstream out;
+    test_sheet.set_selection(new Select_Contains(&test_sheet, "Age", "19"));
+    test_sheet.print_selection(out);
+    EXPECT_EQ(out.str(), "Angel Adams 19 English \nAmy Adams 19 English \n");
+}
+
 
 #endif //__SELECT_CONTAINS_TEST_HPP__
