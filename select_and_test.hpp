cde090b8a89e51bb392dd6c8291ee5e872a2ff90
alice-thaitea <athai026@ucr.edu>
2021-05-04 11:37:58 -0700

    Wrote and test, filter First Angel and Amy

---
+++ select_and_test.hpp
@@ -0,0 +1,25 @@
+#ifndef __SELECT_AND_TEST_HPP__
+#define __SELECT_AND_TEST_HPP__
+
+#include "gtest/gtest.h"
+#include "selectContains.hpp"
+#include "selectAnd.hpp"
+#include "spreadsheet.hpp"
+#include "spreadsheet.cpp"
+
+TEST(SelectAnd, First_Angel_Amy) {
+    Spreadsheet test_sheet;
+    test_sheet.set_column_names({"First", "Last", "Age", "Major"});
+    test_sheet.add_row({"Angel", "Adams", "19", "English"});
+    test_sheet.add_row({"Charles", "Cornell", "21", "Music"});
+    test_sheet.add_row({"Lauren", "Dang", "20", "Film"});
+    test_sheet.add_row({"Amy", "Adams", "19", "English"});
+ 
+    std::ostringstream out;
+    test_sheet.set_selection(new Select_And(new Select_Contains(&test_sheet, "First", "Angel"), new Select_Contains(&test_sheet, "First", "Amy")));
+    test_sheet.print_selection(out);
+    EXPECT_EQ(out.str(), "");
+}
+
+
+#endif //__SELECT_AND_TEST_HPP__
