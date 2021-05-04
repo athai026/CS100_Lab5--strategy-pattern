bcf55cbe18084d1111408f5da0cdad7993a72217
alice-thaitea <athai026@ucr.edu>
2021-05-03 23:38:47 -0700

    Fixed Select_Contains class

--- selectContains.hpp
+++ selectContains.hpp
@@ -6,17 +6,15 @@
 class Select_Contains : public Select {
     protected:
         std::string match;
-        Spreadsheet* spread;
         int column_num;
     public:
-        Select_Contains(Spreadsheet &sheet, const std::string column_name, const std::string s) {
+        Select_Contains(Spreadsheet *sheet, const std::string column_name, const std::string s) {
             match = s;
-            *spread = sheet;
-            column_num = spread->get_column_by_name(column_name);
+            column_num = sheet->get_column_by_name(column_name);
         }
 
-        virtual bool select(const Spreadsheet* spread, int row) const {
-            std::string cell = spread->cell_data(row, column_num);
+        virtual bool select(const Spreadsheet* sheet, int row) const {
+            std::string cell = sheet->cell_data(row, column_num);
             int found = cell.find(match);
             if (found != std::string::npos) {
                 return true;
