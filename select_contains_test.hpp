c27960be51c432154cfb7b57125d66ff3ac71995
alice-thaitea <athai026@ucr.edu>
2021-05-04 01:55:50 -0700

    Wrote containts test, filter First

--- select_contains_test.hpp
+++ select_contains_test.hpp
@@ -35,5 +35,19 @@ TEST(SelectContains, First_A) {
     EXPECT_EQ(out.str(), "Angel Adams 19 English \nAmy Adams 19 English \n");
 }
 
+TEST(SelectContains, First_allnames) {
+    Spreadsheet test_sheet;
+    test_sheet.set_column_names({"First", "Last", "Age", "Major"});
+    test_sheet.add_row({"Angel", "Adams", "19", "English"});
+    test_sheet.add_row({"Charles", "Cornell", "21", "Music"});
+    test_sheet.add_row({"Lauren", "Dang", "20", "Film"});
+    test_sheet.add_row({"Amy", "Adams", "19", "English"});
+ 
+    std::ostringstream out;
+    test_sheet.set_selection(new Select_Contains(&test_sheet, "First", ""));
+    test_sheet.print_selection(out);
+    EXPECT_EQ(out.str(), "Angel Adams 19 English \nCharles Cornell 21 Music \nLauren Dang 20 Film \nAmy Adams 19 English \n");
+}
+
 
 #endif //__SELECT_CONTAINS_TEST_HPP__
